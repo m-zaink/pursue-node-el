bf86ac62715e6b2df2a45a1e9e0943280786a18e
Mohammed Sadiq <sidsmith9@gmail.com>
2019-03-20 13:09:27 +0530

    Change the port to dynamic

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const Joi = require('joi');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const config = require('config');
-const startupDebugger = require('debug')('app:startup');
-const dbDebugger = require('debug')('app:db');
+// const startupDebugger = require('debug')('app:startup');
+// const dbDebugger = require('debug')('app:db');
 
 //  User - Defined
 const courses = require('./routes/courses');
@@ -26,7 +26,6 @@ app.use(express.static('public'));
 app.use(helmet());
 app.use('/api/courses', courses);
 app.use('/', home);
-
 // -------------------------------------
 
 // --------- Configuration -------------
@@ -34,20 +33,20 @@ app.use('/', home);
 // console.log('Application name : ' + config.get('name'));
 // console.log('Mail Server')
 // console.log(`Password : ${config.get('mail.password')}`);
-if (app.get('env') === 'development') {
-    app.use(morgan('tiny'));
-    startupDebugger('Started morgan');
-}
+// if (app.get('env') === 'development') {
+//     app.use(morgan('tiny'));
+//     // startupDebugger('Started morgan');
+// }
 
 // DB work
-dbDebugger('Connected to db')
+// dbDebugger('Connected to db')
 app.set('view engine', 'pug');  // Tells what view engine to use
 app.set('views', './views');    // Tells where to look for the views
 // -------------------------------------
 
 // Listener set - up
 // PORT
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${port}`));
 
 // ------------------------------------
